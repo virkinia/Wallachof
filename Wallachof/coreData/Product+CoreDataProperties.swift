a7a7e2b6c3ec0b229065bc179ea8bf9f16bb1052
Virginia <virlli_nia@hotmail.com>
2019-05-20 17:47:53 +0200

    3. CoreData: Asignar clases a la entidad producto

---
+++ Wallachof/coreData/Product+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Product+CoreDataProperties.swift
+//  Wallachof
+//
+//  Created by Dev2 on 20/05/2019.
+//  Copyright © 2019 CFTIC. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Product {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Product> {
+        return NSFetchRequest<Product>(entityName: "Product")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var price: Float
+    @NSManaged public var publishDate: NSDate?
+    @NSManaged public var sold: Bool
+    @NSManaged public var thumb: NSData?
+    @NSManaged public var desc: String?
+
+}
