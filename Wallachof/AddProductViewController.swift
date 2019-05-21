198808ebfb71d898bd1fa9888797f91d1c513450
Virginia <virlli_nia@hotmail.com>
2019-05-21 17:45:53 +0200

    7. Añadir foto de cámara o galería. Si la cámara no está disponible, tirar de la galería sin preguntar

--- Wallachof/AddProductViewController.swift
+++ Wallachof/AddProductViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class AddProductViewController: UIViewController {
 
+    enum ImageSource {
+        case photoLibrary
+        case camera
+    }
+
+    var imagePicker: UIImagePickerController!
+
+
     @IBOutlet weak var imgProduct: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,30 +27,70 @@ class AddProductViewController: UIViewController {
     
     @IBAction func tapDetected(_ sender: UITapGestureRecognizer) {
         debugPrint("Tap en la imagen \(sender.numberOfTouches)")
-
         showPictureSourceAlert()
     }
 
     func showPictureSourceAlert() {
-    let alert = UIAlertController(title: nil, message: nil, preferredStyle:.actionSheet)
 
-        let cameraAction = UIAlertAction(title: "Cámara", style: .default) { (alert) in
-            //TODO: Abrir Camara
-        }
-        alert.addAction(cameraAction)
-        let albumAction = UIAlertAction(title: "Galería", style: .default){ (alert) in
-            //TODO: Abrir Camara
-        }
-        alert.addAction(albumAction)
-        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel) { (alert) in
-            //TODO: Abrir Camara
-        }
-        alert.addAction(cancelAction)
+        if(UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            let alert = UIAlertController(title: nil, message: nil, preferredStyle:.actionSheet)
 
-        present(alert, animated: true)
+            let cameraAction = UIAlertAction(title: "Cámara", style: .default) { (alert) in
+                //TODO: Abrir Camara
+                self.selectImageFrom(.camera)
+            }
+            alert.addAction(cameraAction)
+            let albumAction = UIAlertAction(title: "Galería", style: .default){ (alert) in
+                //TODO: Abrir Camara
+                  self.selectImageFrom(.photoLibrary)
+
+            }
+            alert.addAction(albumAction)
+            let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel) { (alert) in
+                //TODO: Abrir Camara
+
+            }
+            alert.addAction(cancelAction)
+
+            present(alert, animated: true)
+            }
+        else {
+                selectImageFrom(.photoLibrary)
+            }
+    }
+
+    func selectImageFrom(_ source: ImageSource) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        switch source {
+        case .camera:
+            imagePicker.sourceType = .camera
+        case .photoLibrary:
+                imagePicker.sourceType = .photoLibrary
+        }
+
+        present(imagePicker, animated: true)
 
     }
 
 
+
+}
+extension AddProductViewController:UINavigationControllerDelegate {}
+extension AddProductViewController: UIImagePickerControllerDelegate {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        // 1. cerrar el picker controller
+            picker.dismiss(animated: true)
+        // 2. Coger la imagen (info es un diccionario con claves enumeradas del tipo UIImagePickerController.InfoKey)
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            debugPrint("Error no se puede conseguir la iamgen")
+            return
+        }
+
+        imgProduct.image = selectedImage
+
+    }
 
 }
